034f7f52937640a3bc9283f46c015dfc5e435e77
myALKleon <myalkleon@gmail.com>
2019-11-20 03:59:04 +0300

    Downloaded all files

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+var options1 = {
+  strings: ['artem'],
+  typeSpeed: 100,
+  showCursor: false,
+};
+
+var typed = new Typed('.typing-imitation-1', options1);
+
+var options2 = {
+  strings: ['Lomakin'],
+  typeSpeed: 200,
+};
+
+var typed = new Typed('.typing-imitation-2', options2);
