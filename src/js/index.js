f3651849e4f2b22f4aed2381e8cef0ed0e0cbadd
myALKleon <myalkleon@gmail.com>
2019-12-10 09:26:36 +0300

    Preloader added

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,35 @@
-var options1 = {
-  strings: ['artem'],
-  typeSpeed: 100,
-  showCursor: false,
+function imitateTyping() {
+  var options1 = {
+    strings: ['artem'],
+    typeSpeed: 100,
+    showCursor: false,
+  };
+  var typed = new Typed('.typing-imitation-1', options1);
+  
+  var options2 = {
+    strings: ['Lomakin'],
+    typeSpeed: 200,
+  };
+  var typed = new Typed('.typing-imitation-2', options2);
+}
+
+function fadeOut(element, animTime) {  
+  element.style.transition = `opacity ${animTime}ms`;
+  element.style.opacity = '0';
+  setTimeout(() => {
+    element.style.display = 'none';
+  }, animTime);
+}
+
+document.body.style.overflow = 'hidden';
+window.onload = () => {
+  let onloader = document.getElementById('onloader');
+  setTimeout(() => {
+    try {
+      fadeOut(onloader, 350);
+      setTimeout(imitateTyping, 350);
+    } finally {
+      document.body.style.overflow = 'visible';
+    }
+  }, 500);  
 };
-
-var typed = new Typed('.typing-imitation-1', options1);
-
-var options2 = {
-  strings: ['Lomakin'],
-  typeSpeed: 200,
-};
-
-var typed = new Typed('.typing-imitation-2', options2);
